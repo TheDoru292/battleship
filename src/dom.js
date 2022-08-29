6b40f6c0fd5fdcacb220cde996745075ac496f7d
TheDoru <thedoruv2@gmail.com>
2022-08-29 22:38:31 +0200

    Created placeShipOnGameboard function

--- src/dom.js
+++ src/dom.js
@@ -3,7 +3,7 @@ function createGameboard(playerName, object) {
 
   const container = document.createElement("div");
   container.className = "gameboard";
-  container.dataset.gameboardPlayer = `${playerName}`;
+  container.dataset.gameboard = `${playerName}`;
 
   for (let i = 0; object.getGameboard().getGameboardSize() > i; i++) {
     const cell = document.createElement("div");
@@ -15,4 +15,40 @@ function createGameboard(playerName, object) {
   mainContainer.append(container);
 }
 
-export { createGameboard };
+function placeShipOnGameboard(playerName, shipName, startPoint, length) {
+  const container = document.querySelector(`[data-gameboard="${playerName}"]`);
+
+  for (let i = 0; 100 > i; i++) {
+    let n = startPoint + length;
+    if (inRange(i, startPoint, n - 1) === true) {
+      let children = container.children;
+
+      for (let j = 0; children.length > j; j++) {
+        let cellId = children[j].getAttribute("data-cell-id");
+
+        if (cellId == i) {
+          const shipSomething = document.createElement("p");
+          shipSomething.textContent = `${shipName[0]}`;
+          shipSomething.dataset.ship = shipName;
+          shipSomething.dataset.shipCell = i;
+
+          children[j].append(shipSomething);
+
+          continue;
+        }
+        if (cellId > i) {
+          break;
+        }
+      }
+    }
+    if (i === 100 || i > n) {
+      break;
+    }
+  }
+}
+
+function inRange(x, min, max) {
+  return (x - min) * (x - max) <= 0;
+}
+
+export { createGameboard, placeShipOnGameboard };
